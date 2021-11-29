4a6b16e05127d56660e083484a9f41dff88947e6
Cariad Eccleston <cariad@cariad.earth>
2021-11-29 19:45:03 +0000

    Remove outlines (#3)

--- comprehemd/__init__.py
+++ comprehemd/__init__.py
@@ -2,9 +2,7 @@ import importlib.resources as pkg_resources
 
 from comprehemd.blocks import Block, CodeBlock, EmptyBlock, HeadingBlock
 from comprehemd.fence import Fence
-from comprehemd.outline import Outline, OutlineItem
 from comprehemd.parser import MarkdownParser
-from comprehemd.read_outline import read_outline
 
 with pkg_resources.open_text(__package__, "VERSION") as t:
     __version__ = t.readline().strip()
@@ -15,8 +13,5 @@ __all__ = [
     "EmptyBlock",
     "Fence",
     "HeadingBlock",
-    "Outline",
-    "OutlineItem",
-    "read_outline",
     "MarkdownParser",
 ]
